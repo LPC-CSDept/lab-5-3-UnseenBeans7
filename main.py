e9175549fc0beb660ab4b14b02cdeff5c8143bc2
Kyuwoong Lee <kwooler@gmail.com>
2022-10-16 20:55:13 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
 
-def getinput(n1, n2):
-# 	print(id(n1))
-	n1 = input('Enter a number')
-	n2 = input('Enter a number')
+def mineven(numbers):
+	# make your code
 
-def main():
-	num1 = num2 = 0 
-	getinput(num1, num2)
-	print (num1, num2)
 
-if __name__ == '__main__':
-	main()
+
+
+
