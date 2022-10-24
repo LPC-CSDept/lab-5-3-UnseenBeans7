91246abe57574a0f83a00914ecc9f5ae9c09f7e4
Kyu Lee <kwooler@gmail.com>
2022-10-24 01:57:28 -0700

    Lab 03

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
+import random
+
 
 def mineven(numbers):
-	# make your code
+    ##################################################
+    # make your code
+    ##################################################
+    return minval
+
+
+numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+result = mineven(numbers)
+print(numbers)
+
+numbers = [1, 3, 5, 7, 10, 14, 2, 8]
+result = mineven(numbers)
+print(numbers)
+
+
+N = random.randint(5, 20)
+numbers = [random.randint(-50, 50) for v in range(N)]
+print(numbers)
+ret = mineven(numbers)
+print(numbers)
+print(ret)
--- main_test.py
+++ main_test.py
@@ -2,16 +2,16 @@ import random
 import main
 
 
-def mineven(numbers):
-    evenlst = [v for v in numbers if v % 2 == 0]
-    return min(evenlst)
-
-
 def test_main():
     N = random.randint(5, 20)
     numbers = [random.randint(-50, 50) for v in range(N)]
-    print(numbers)
-    target = mineven(numbers)
+    print('Original List', numbers)
+    elst = [v for v in numbers if v % 2 == 0]
+    target = min(elst)
+    targetlen = len(numbers) - 1
     ret = main.mineven(numbers)
-    print(ret)
-    assert ret == target
+    print('Your function return this value: ', ret)
+    print('After function call, Numbers: ', numbers)
+    assert ret == target, 'Wrong return value '
+
+    assert targetlen == len(numbers), 'The length of list is not correct'
