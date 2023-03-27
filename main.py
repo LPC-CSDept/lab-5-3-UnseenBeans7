88efd6e6b34548338cadbc171974616916ba9473
Kyu Lee <kwooler@gmail.com>
2023-03-26 21:23:25 -0700

    L53

--- main.py
+++ main.py
@@ -5,21 +5,28 @@ def mineven(numbers):
     ##################################################
     # make your code
     ##################################################
+
     return minval
 
 
-numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-result = mineven(numbers)
-print(numbers)
+def main():
+    numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    result = mineven(numbers)
+    print(numbers)
+    print(result)
 
-numbers = [1, 3, 5, 7, 10, 14, 2, 8]
-result = mineven(numbers)
-print(numbers)
+    numbers = [1, 3, 5, 7, 10, 14, 2, 8]
+    result = mineven(numbers)
+    print(numbers)
+    print(result)
+
+    N = random.randint(5, 20)
+    numbers = [random.randint(-50, 50) for v in range(N)]
+    print(numbers)
+    result = mineven(numbers)
+    print(numbers)
+    print(result)
 
 
-N = random.randint(5, 20)
-numbers = [random.randint(-50, 50) for v in range(N)]
-print(numbers)
-ret = mineven(numbers)
-print(numbers)
-print(ret)
+if __name__ == '__main__':
+    main()
